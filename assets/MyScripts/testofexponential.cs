04a8a50dd9fa490da2e5e92b7e20996f3e326113
Petr <essoll@gmail.com>
2014-06-18 18:09:20 +0200

    added test for possoin and exponential so we can expoert the values and gave them to kris

--- assets/MyScripts/testofexponential.cs
+++ assets/MyScripts/testofexponential.cs
@@ -6,12 +6,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-public class testofpoisson : MonoBehaviour
+public class testofexponential : MonoBehaviour
 {
 	private UnityRandom urand;
 	
 	private int lauf =0;
-	private StreamWriter possion ;
+	private StreamWriter exponential ;
 
 
 
@@ -19,23 +19,23 @@ public class testofpoisson : MonoBehaviour
 	{
 		urand = new UnityRandom(9000);
 
-		possion = new StreamWriter ("Possion.txt");
+		exponential = new StreamWriter ("Exponential.txt");
 		//sw1 = new StreamWriter ("Possion.txt")
-		possion.WriteLine ("Possion function with lambda 5.0f and the seed 9000 ");
+		exponential.WriteLine ("exponential function with lambda 5.0f and the seed 9000 ");
 		//sw1.WriteLine ("Possion function with lambda 5.0f and the seed 9000 ");
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		possion.WriteLine(urand.Possion (5.0f)); 
+		exponential.WriteLine(urand.Exponential (5.0f)); 
 		//sw1.WriteLine (urand.Possion (5.0f));
 	}
 
 
 void OnApplicationQuit() {
 	
-		possion.Close ();
+		exponential.Close ();
 }
 
 }
