09db83cf4a9f1dbafd8de101e68c3b14304d05df
Axel <aschaffland@uos.de>
2014-06-26 13:11:19 +0200

    pause state implemented changes in asssets needed

--- assets/MyScripts/ManagerScript.cs
+++ assets/MyScripts/ManagerScript.cs
@@ -2,60 +2,71 @@
 using System.Collections.Generic;
 using System.IO;
 
-public class ManagerScript : MonoBehaviour {
+public class ManagerScript : MonoBehaviour
+{
 
-	// states for state machine to describe in which experiment state we are
-	public enum states { startScreen, walking, pointing, questionaire};
+		// states for state machine to describe in which experiment state we are
+		public enum states
+		{
+				startScreen,
+				walking,
+				pointing,
+				questionaire,
+				pause}
+		;
 
-	// chiffre for identification, can be changed in start screen
-	public static string chiffre = "";
+		// chiffre for identification, can be changed in start screen
+		public static string chiffre = "";
+		public static states state;
 
-	public static states state;
+		//public list of trials
+		public static List<trialContainer> trialList = new List<trialContainer> ();
+		//static variable tracks what trial is in process
+		public static int trialNumber = 0 ;
+		public static string trialFolder = Application.dataPath + @"\Trial" + (System.DateTime.Now).ToString ("MMM-ddd-d-HH-mm-ss-yyyy");
+		public static bool trialINprocess = false;
+		public static bool pointTaskINprocess = false;
 
-	//public list of trials
-	public static List<trialContainer> trialList = new List<trialContainer>();
-	//static variable tracks what trial is in process
-	public static int trialNumber = 0 ;
-		
-	public static string trialFolder =  Application.dataPath+@"\Trial"+(System.DateTime.Now).ToString("MMM-ddd-d-HH-mm-ss-yyyy");
+		//Trials and random variables will be generated here
+		void Awake ()
+		{
+				trialINprocess = true;
 
-	public static bool trialINprocess = false;
+				if (!Directory.Exists (ManagerScript.trialFolder)) {
+						Directory.CreateDirectory (ManagerScript.trialFolder);
+				}
 
-	public static bool pointTaskINprocess = false;
-
-	//Trials and random variables will be generated here
-	void Awake(){
-		trialINprocess = true;
-
-		if (!Directory.Exists(ManagerScript.trialFolder)){
-			Directory.CreateDirectory(ManagerScript.trialFolder);
+				//adding trials to the list
+				//Generate random values for conditions
+				for (int i=0; i<5; i++) {
+						trialContainer tempTrial = new trialContainer ();
+						tempTrial.lightColor = "red";
+						trialList.Add (tempTrial);
+				}
 		}
-
-		//adding trials to the list
-		//Generate random values for conditions
-		for (int i=0; i<5; i++) {
-			trialContainer tempTrial = new trialContainer();
-			tempTrial.lightColor = "red";
-			trialList.Add (tempTrial);
+		//
+		void Start ()
+		{
+				ManagerScript.switchState (states.startScreen);
 		}
-	}
-	//
-	void Start () {
-		ManagerScript.switchState (states.startScreen);
-	}
 	
-	// Update is called once per frame
-	void Update () {
+		// Update is called once per frame
+		void Update ()
+		{
+				Debug.Log (state.ToString ());
 	
-	}
-
-	public static void abortTrial (){
-		trialNumber++;
-		trialINprocess = false;
-		CameraFade.StartAlphaFade (Color.black, false, 2f, 2f, () => {Application.LoadLevel (0); });
 		}
 
-	public static void switchState (states newState){
+		public static void abortTrial ()
+		{
+				trialNumber++;
+				trialINprocess = false;
+				CameraFade.StartAlphaFade (Color.black, false, 2f, 2f, () => {
+						Application.LoadLevel (0); });
+		}
+
+		public static void switchState (states newState)
+		{
 				switch (newState) {
 				case states.startScreen:
 						Time.timeScale = 0;
@@ -64,6 +75,9 @@ public class ManagerScript : MonoBehaviour {
 				case states.walking:
 						Time.timeScale = 1;
 						ManagerScript.state = states.walking;
+						break;
+				case states.pause:
+						ManagerScript.state = states.pause;
 						break;
 				}
 		}
