9a246b59f937eae2b78b4d34a5147138d676c59f
Petr <essoll@gmail.com>
2014-06-27 11:23:02 +0200

    adding crosshair

---
+++ assets/MyScripts/crosshairtest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class crosshairtest : MonoBehaviour 
+{
+	
+	public Texture2D m_crosshairTexture;
+	// Rect for crosshair size and position
+	private Rect m_crosshairRect;  
+	
+	// bool to turn crosshair on and off
+	private bool m_bIsCrosshairVisible = true;
+	
+	void Awake () 
+	{
+		m_crosshairRect = new Rect((Screen.width - m_crosshairTexture.width) / 2, 
+		                           (Screen.height - m_crosshairTexture.height) / 2, 
+		                           m_crosshairTexture.width, 
+		                           m_crosshairTexture.height);
+	}
+	
+	void OnGUI () 
+	{
+		if(m_bIsCrosshairVisible)
+			GUI.DrawTexture(m_crosshairRect, m_crosshairTexture);
+	}
+}
