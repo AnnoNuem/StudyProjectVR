4901d5e0f6eb98d67ba5e717f44272bd93c61e91
Munis Quadri <munis_q@hotmail.com>
2014-07-27 15:35:46 +0200

    Data structure updates

--- assets/MyScripts/recordCoordinates.cs
+++ assets/MyScripts/recordCoordinates.cs
@@ -10,6 +10,8 @@ public class recordCoordinates : MonoBehaviour {
 
 	//Use to store file path
 	public static string filePath;
+	public static string filePath1;
+	string delimiter = ",";
 
 	//for degree measurment
 	public float angleBetween = 999.0F;
@@ -23,6 +25,37 @@ public class recordCoordinates : MonoBehaviour {
 
 	//FixedUpdate is used for rigid bodies and is executed independently of the configuration of system i.e. runs on regular set of intervals
 	void FixedUpdate () {
+
+		/*
+		filePath1 = ManagerScript.trialFolder+ "/Trial-"+ ManagerScript.trialNumber +"-Flow.csv";
+		string delimiter = ",";
+
+		if (!File.Exists (filePath)) {
+						File.Create (filePath).Close ();
+				}
+
+		string[][] output1 = new string[][]{
+			new string[]{
+				//ManagerScript.trialNumber.ToString (),
+				(transform.position.x).ToString(),
+				(transform.position.y).ToString(),
+				(transform.position.z).ToString(),}
+				//(transform.forward).ToString(),
+				//(Time.realtimeSinceStartup).ToString(),
+				//(Time.deltaTime).ToString(),
+				//(angleBetween).ToString(),
+				//ManagerScript.CondtionTypeVariableInContainer} 
+		};
+		
+		int length1 = output1.GetLength(0);
+		
+		StringBuilder sb1 = new StringBuilder();
+		
+		for (int index = 0; index < length1; index++)
+			sb1.AppendLine(string.Join(delimiter, output1[index]));
+		File.AppendAllText(filePath1, sb1.ToString());
+
+		*/
 
 		//code will only execute when K is pressed
 		if (Input.GetKeyDown (KeyCode.K) && ManagerScript.state==ManagerScript.states.pointing) {
@@ -38,19 +71,21 @@ public class recordCoordinates : MonoBehaviour {
 			Vector2 targetDir = targetVector - transformVector;
 			angleBetween = Vector2.Angle (targetDir, forwardVector);
 			keyPressedK = true;
-			Debug.Log("ManagerScript.trialINprocess value -->"+ManagerScript.trialINprocess);
+			//Debug.Log("ManagerScript.trialINprocess value -->"+ManagerScript.trialINprocess);
 
 			//flag to enable new CSV for each trial
 			//if(ManagerScript.trialINprocess){
-				filePath = ManagerScript.trialFolder+ "/Trial"+ManagerScript.trialNumber+".csv";
+				//filePath = ManagerScript.trialFolder+ "/Trial"+ManagerScript.trialNumber+".csv";
+				filePath = ManagerScript.trialFolder+ "/Trial-Error-Angles.csv";
 				Debug.Log ("File Path -->" + filePath);
 
-				string delimiter = ",";
+				//string delimiter = ",";
 				
 				//Check if the file exists ( Not really needed here)
 				if (!File.Exists(filePath)) {
 					File.Create(filePath).Close();
-
+					
+				/*
 					//putting values for column in csv
 					string[][] output1 = new string[][]{
 					new string[]{"x-pos","y-pos","transform-z","transform-forward","time-startup","time-delta","Error angle","State"} 
@@ -63,13 +98,41 @@ public class recordCoordinates : MonoBehaviour {
 					for (int index = 0; index < length1; index++)
 						sb1.AppendLine(string.Join(delimiter, output1[index]));
 					File.AppendAllText(filePath, sb1.ToString());
-					
+				*/	
 				}	
-				
+
+			string conditionVal = "";
+			
+			if (ManagerScript.CondtionTypeVariableInContainer == "Easy") {
+				conditionVal = "1";
+			} else if (ManagerScript.CondtionTypeVariableInContainer == "Hard") {
+				conditionVal = "2";
+			} else if (ManagerScript.CondtionTypeVariableInContainer == "Easy-False") {
+				conditionVal = "3";
+			} else if (ManagerScript.CondtionTypeVariableInContainer == "Hard-False") {
+				conditionVal = "4";
+			} else if (ManagerScript.CondtionTypeVariableInContainer == "Training") {
+				conditionVal = "5";
+			} else if (ManagerScript.CondtionTypeVariableInContainer == "ENDTRIAL") {
+				conditionVal = "6";
+			} else {
+				conditionVal = "0";
+			}
 				
 				//putting values for column in csv
 				string[][] output = new string[][]{
-					new string[]{(transform.position.x).ToString(),(transform.position.y).ToString(),(transform.position.z).ToString(),(transform.forward).ToString(),(Time.realtimeSinceStartup).ToString(),(Time.deltaTime).ToString(),(angleBetween).ToString(),ManagerScript.CondtionTypeVariableInContainer} 
+					new string[]{
+					ManagerScript.trialNumber.ToString (),
+					//(transform.position.x).ToString(),
+					//(transform.position.y).ToString(),
+					//(transform.position.z).ToString(),
+					//(transform.forward).ToString(),
+					(Time.realtimeSinceStartup).ToString(),
+					//(Time.deltaTime).ToString(),
+					(angleBetween).ToString(),
+					conditionVal
+					//ManagerScript.CondtionTypeVariableInContainer
+				} 
 				};
 				
 				int length = output.GetLength(0);
--- assets/customUtils/recordData.cs
+++ assets/customUtils/recordData.cs
@@ -20,7 +20,8 @@ public static class recordData
 						ManagerScript.parameterFile = filePath;
 				}
 				//"Trial Number","Spawn Distance","Cool Down","Timer Red","Color","Generated Angle"
-
+				
+				/*
 				string[][] output = new string[][]{
 				new string[] {
 							"Trial Number",
@@ -39,22 +40,42 @@ public static class recordData
 				for (int index = 0; index < length; index++)
 						sb.AppendLine (string.Join (delimiter, output [index]));
 				File.AppendAllText (filePath, sb.ToString ());
-			
+				*/
 		}
 		
 		//records the paramters
 		public static void recordDataParameters ()
 		{
+
+				string conditionVal = "";
+
+				if (ManagerScript.CondtionTypeVariableInContainer == "Easy") {
+						conditionVal = "1";
+				} else if (ManagerScript.CondtionTypeVariableInContainer == "Hard") {
+						conditionVal = "2";
+				} else if (ManagerScript.CondtionTypeVariableInContainer == "Easy-False") {
+						conditionVal = "3";
+				} else if (ManagerScript.CondtionTypeVariableInContainer == "Hard-False") {
+						conditionVal = "4";
+				} else if (ManagerScript.CondtionTypeVariableInContainer == "Training") {
+						conditionVal = "5";
+				} else if (ManagerScript.CondtionTypeVariableInContainer == "ENDTRIAL") {
+						conditionVal = "6";
+				} else {
+						conditionVal = "0";
+				}
+
 				//putting values for column in csv
 				string[][] output = new string[][]{
 			new string[] {
 						ManagerScript.trialNumber.ToString (),
 						ManagerScript.spawnDistance.ToString (),
-						ManagerScript.CoolDown.ToString (),
-						ManagerScript.timer_red.ToString (),
-						ManagerScript.bColor.ToString (),
+				//ManagerScript.CoolDown.ToString (),
+				//ManagerScript.timer_red.ToString (),
+				//ManagerScript.bColor.ToString (),
 						ManagerScript.generatedAngle.ToString (),
-					    ManagerScript.CondtionTypeVariableInContainer
+						conditionVal,
+						
 				} 
 		};
 
@@ -70,13 +91,14 @@ public static class recordData
 		
 		// Updates the csv for stressors with marker of spawned , destroyed or missed
 		public static void recordDataStressors (string status)
-		{
-				string filePath = ManagerScript.trialFolder + "/Trial" + ManagerScript.trialNumber + "-Stressors.csv";
-
+		{		
+				//string filePath = ManagerScript.trialFolder + "/Trial" + ManagerScript.trialNumber + "-Stressors.csv";
+				string filePath = ManagerScript.trialFolder + "/Trial-Stressors.csv";
 				//Check if the file exists
 				if (!File.Exists (filePath)) {
 						File.Create (filePath).Close ();
 						
+						/*
 						//putting values for column in csv
 						string[][] output1 = new string[][]{
 							new string[]{"Time-startup","status"} 
@@ -89,14 +111,26 @@ public static class recordData
 						for (int index = 0; index < length1; index++)
 							sb1.AppendLine (string.Join (delimiter, output1 [index]));
 						File.AppendAllText (filePath, sb1.ToString ());
-						
+						*/
 				}
 
+				string statusVal = "";
+
+				if (status == "S") {
+						statusVal = "0";
+				} else if (status == "M") {
+						statusVal = "1";
+				} else {
+						statusVal = "2";
+				}
 
 				//putting values for column in csv
 				string[][] output = new string[][]{
-			new string[]{(Time.realtimeSinceStartup).ToString (),status} 
-		};
+					new string[]{
+					ManagerScript.trialNumber.ToString (),
+					(Time.realtimeSinceStartup).ToString (),
+					statusVal} 
+				};
 		
 				int length = output.GetLength (0);
 		
@@ -106,5 +140,39 @@ public static class recordData
 						sb.AppendLine (string.Join (delimiter, output [index]));
 				File.AppendAllText (filePath, sb.ToString ());
 		}
+	/*
+		public static void recordDataFlow ()
+		{
+				
+				string filePath = ManagerScript.trialFolder + "/Trial-" + ManagerScript.trialNumber + "-Flow.csv";
+				if (!File.Exists (filePath)) {
+						File.Create (filePath).Close ();
+				}
 
+				//putting values for column in csv
+				string[][] output = new string[][]{
+			new string[]{
+				//ManagerScript.trialNumber.ToString (),
+				(transform.position.x).ToString(),
+				(transform.position.y).ToString(),
+				(transform.position.z).ToString()
+				//(transform.forward).ToString(),
+				//(Time.realtimeSinceStartup).ToString (),
+				//(Time.deltaTime).ToString(),
+				//(angleBetween).ToString (),
+				//conditionVal
+				//ManagerScript.CondtionTypeVariableInContainer
+			} 
+		};
+		
+				int length = output.GetLength (0);
+		
+				StringBuilder sb = new StringBuilder ();
+		
+				for (int index = 0; index < length; index++)
+						sb.AppendLine (string.Join (delimiter, output [index]));
+				File.AppendAllText (filePath, sb.ToString ());
+
+		}
+		*/
 }
