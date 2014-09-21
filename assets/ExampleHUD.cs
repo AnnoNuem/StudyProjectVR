5b8fa329d5d0f45296574c1c98b4ba8284b5b51e
razial <essoll@gmail.com>
2014-09-21 13:31:39 +0200

    added vrgui assets

---
+++ assets/ExampleHUD.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleHUD : VRGUI 
+{
+	public GUISkin skin;
+
+	int count = 0;
+
+	public override void OnVRGUI()
+	{
+		GUI.skin = skin;
+
+		// NG 
+		//GUI.Label(new Rect(0f, 0f, 600f, 100f), "<color=lime>Time: " +  Time.time+"</color> ");
+		// OK
+		GUI.Button(new Rect(100f, 10f, 600f, 100f), " <color=lime> Time: " +  Time.time+"</color> ");
+
+		/*if(GUI.Button (new Rect (0, 0, Screen.width, Screen.height), "<color=red>This is Screen Size Button  ["  +count +"]</color>"))
+		{
+			count++;
+		}*/
+
+
+		GUILayout.BeginArea (new Rect (0, 0, Screen.width, Screen.height));
+		GUILayout.BeginVertical ("box");
+		if (GUILayout.Button ("---")) {
+			count--;
+		}
+		if (GUILayout.Button ("+++")) {
+			count++;
+		}
+
+		GUILayout.EndVertical ();
+
+		GUILayout.BeginHorizontal();
+		GUILayout.Label ("<color=lime>[ "+count+" ]</color>");
+
+		if (GUILayout.Button ("Reset",GUILayout.ExpandHeight(true))) {
+			count=0;
+		}
+		GUILayout.EndHorizontal();
+
+
+		GUILayout.EndArea ();
+
+
+
+
+	}
+}
