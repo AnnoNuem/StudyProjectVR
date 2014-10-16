5569e5a1fa00f4bacabf7f4a4894e4621e49c38b
Pc Uni <herrpeterpaul@web.de>
2014-10-16 18:23:58 +0200

    yellow ball now spawning in front of player

--- assets/MyScripts/SpawnLookRed.cs
+++ assets/MyScripts/SpawnLookRed.cs
@@ -5,21 +5,20 @@ using XInputDotNetPure;
 public class SpawnLookRed : MonoBehaviour
 {
 		GameObject displaytext ;
-
-	Vector3 v;
-	Ray r;
-	float rotationSpeed = 130f;
-	float transformationSpeed = 15f;
-	float distanceToGoal = 10;
-
+	float random;
+		Vector3 v;
+		Ray r, r2;
+		float rotationSpeed = 130f;
+		float transformationSpeed = 15f;
+		float distanceToGoal = 10;
 		bool spawning_red = true ; // a variable for controlling processes
-		public int spawnDistance = 40 ;
+		public float spawnDistance = 40f ;
 		float spawnheight = 20f;
 		public double CoolDown = 2.0;       // How long to hide
 		public float timer_red = 0.0f; // timer, than needs to reach CoolDown
 		public float TimerForLooking = 0.0f; // timer, than needs to reach CoolDownValue
 		public int moveDistance = 5;   // How close can the character get
-	//	public float speed = 5.0f ; // the speed of the sphere
+		//	public float speed = 5.0f ; // the speed of the sphere
 		private UnityRandom urand;
 		Vector3 pos;
 		private int timeTillExp = 1; // how long till explosion
@@ -39,9 +38,7 @@ public class SpawnLookRed : MonoBehaviour
 		float TimerFromSpawn;
 		float TimerAfterSetOn;
 		bool CanBeDefeated;
-
 		bool vibrate;
-
 		GameObject pController;
 		public MonoBehaviour characterMotor;
 		
@@ -88,41 +85,41 @@ public class SpawnLookRed : MonoBehaviour
 				CondtionTypeVariableInContainer = ManagerScript.CondtionTypeVariableInContainer;
 
 				// if the state is walking, lets render the shit out of it. 
-		if (ManagerScript.state == ManagerScript.states.walking && CondtionTypeVariableInContainer != "Explain" && CondtionTypeVariableInContainer != "Dummy" && CondtionTypeVariableInContainer != "Training" ) {
+				if (ManagerScript.state == ManagerScript.states.walking && CondtionTypeVariableInContainer != "Explain" && CondtionTypeVariableInContainer != "Dummy" && CondtionTypeVariableInContainer != "Training") {
 
 						// this part is responsable for wating some time and respawn the object
 						if (!renderer.enabled) {
-				//Debug.Log(CondtionTypeVariableInContainer);
-										timer_red += Time.deltaTime;
-										if (timer_red > CoolDown) { 
-												MoveAndShow ();
-												renderer.enabled = true;
-												recordData.recordDataStressors ("S");
-												Pause.ChangeNumberOfYellowSpaw ();
-												TimeToRespand = 0;
-												TimerFromSpawn = 0;
-												TimerAfterSetOn = 0;
-												CanBeDefeated = false;
-												((Crosshairtesting2)(GameObject.Find ("OVRPlayerController").GetComponent ("Crosshairtesting2"))).SmallCrosshair ();
-											//	Debug.Log ("bla");
+								//Debug.Log(CondtionTypeVariableInContainer);
+								timer_red += Time.deltaTime;
+								if (timer_red > CoolDown) { 
+										MoveAndShow ();
+										renderer.enabled = true;
+										recordData.recordDataStressors ("S");
+										Pause.ChangeNumberOfYellowSpaw ();
+										TimeToRespand = 0;
+										TimerFromSpawn = 0;
+										TimerAfterSetOn = 0;
+										CanBeDefeated = false;
+										((Crosshairtesting2)(GameObject.Find ("OVRPlayerController").GetComponent ("Crosshairtesting2"))).SmallCrosshair ();
+										//	Debug.Log ("bla");
 										
 								}
 						}
 		
 						// if object visible move it towards the player =)
 						if (renderer.enabled) {
-				v = character.position;
-				r = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
-				v.x = v.x + r.direction.x * distanceToGoal;
-				v.z = v.z + r.direction.z * distanceToGoal;
-				v.y=7;			
+								v = character.position;
+								r = Camera.main.ViewportPointToRay (new Vector3 (0.5F, 0.5F, 0));
+								v.x = v.x + r.direction.x * distanceToGoal;
+								v.z = v.z + r.direction.z * distanceToGoal;
+								v.y = 7;			
 				
 				
 				
-				transform.position = Vector3.MoveTowards (transform.position, v, (float)(transformationSpeed * Time.deltaTime));
-				//profesional hardcoding to prevent taht zellow spehere flows into ground
+								transform.position = Vector3.MoveTowards (transform.position, v, (float)(transformationSpeed * Time.deltaTime));
+								//profesional hardcoding to prevent taht zellow spehere flows into ground
 
-				transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed );
+								transform.Rotate (Vector3.right * Time.deltaTime * rotationSpeed);
 						}
 
 
@@ -155,8 +152,8 @@ public class SpawnLookRed : MonoBehaviour
 						if (CanBeDefeated) {
 								TimerAfterSetOn += Time.deltaTime;
 								//((Crosshairtesting2)(GameObject.Find ("Character").GetComponent ("Crosshairtesting2"))).BigCrosshair ();
-						//	((Crosshairtesting2)(GameObject.Find ("OVRPlayerController").GetComponent ("Crosshairtesting2"))).BigCrosshair ();
-							displaytext.GetComponent<TextMesh>().text = "SHOOT" ;
+								//	((Crosshairtesting2)(GameObject.Find ("OVRPlayerController").GetComponent ("Crosshairtesting2"))).BigCrosshair ();
+								displaytext.GetComponent<TextMesh> ().text = "SHOOT";
 
 							
 
@@ -166,22 +163,22 @@ public class SpawnLookRed : MonoBehaviour
 			
 						if (TimerAfterSetOn > TimeToRespand) {
 								CanBeDefeated = false;
-			                 	displaytext.GetComponent<TextMesh>().text = " " ;
+								displaytext.GetComponent<TextMesh> ().text = " ";
 
 
- 				//  Crosshairtesting2)(GameObject.Find ("Character").GetComponent ("Crosshairtesting2"))).SmallCrosshair ();
-				//  ((Crosshairtesting2)(GameObject.Find ("OVRPlayerController").GetComponent("Crosshairtesting2"))).SmallCrosshair ();
+								//  Crosshairtesting2)(GameObject.Find ("Character").GetComponent ("Crosshairtesting2"))).SmallCrosshair ();
+								//  ((Crosshairtesting2)(GameObject.Find ("OVRPlayerController").GetComponent("Crosshairtesting2"))).SmallCrosshair ();
 
-							// & renderer.enabled to prevent explosion when the ball is defeated, as it was still expoding.
-							if (!willExplode && renderer.enabled) {
-									willExplode = true;
-									Invoke ("startExp", timeTillExp);
-							}
+								// & renderer.enabled to prevent explosion when the ball is defeated, as it was still expoding.
+								if (!willExplode && renderer.enabled) {
+										willExplode = true;
+										Invoke ("startExp", timeTillExp);
+								}
 						}	
 
 
 						// if you can respond and press the key, the ball is defeaded
-						if ( (Input.GetKeyDown (KeyCode.G) || Input.GetButtonDown("360controllerButtonB") ) && CanBeDefeated) {
+						if ((Input.GetKeyDown (KeyCode.G) || Input.GetButtonDown ("360controllerButtonB")) && CanBeDefeated) {
 								renderer.enabled = false;
 								recordData.recordDataStressors ("D");
 								Pause.ChangeNumberOfYellowDefeted ();
@@ -200,48 +197,33 @@ public class SpawnLookRed : MonoBehaviour
 		
 						}
 
-						} else {
+				} else {
 						// if not in proper state just dissable the rendering and everything is fine
 						renderer.enabled = false;
-		//	Debug.Log("turn off render of ball") ;
+						//	Debug.Log("turn off render of ball") ;
 
-						}		
+				}		
 		}
 
 		// this is the function that respawns the yellow sphere
 		void MoveAndShow ()
 		{	
+				CancelInvoke ("startExp");
 				willExplode = false;
 
 				// here we get a rondom value for the jidder of the onset
 				GenerateTimeOnsetOfDefeatTime ();
 
-
-
-				float temp123 = (float)urand.Range (3, 8, UnityRandom.Normalization.STDNORMAL, 0.1f);
-				pos.x = (temp123 / 10);
-				pos.z = (float)spawnDistance;
-
-				// this does the magic to put it in the left or right upper corner 
-				pos = Camera.main.ViewportToWorldPoint (pos);
-				//randomize the height of the spwan position of the orange sphere between 4 and 12
-
-				//pos.y = Random.Range (4, 13);
+				random = (float)urand.Range (-10, 10, UnityRandom.Normalization.STDNORMAL, 0.1f);
+				r2 = Camera.main.ViewportPointToRay (new Vector3 (0.5F, 0.5F, 0));
+				pos.x = (character.position.x + r2.direction.x * spawnDistance) + random;
+				pos.z = (character.position.z + r2.direction.z * spawnDistance) - random;
 				pos.y = spawnheight;
-				//apply new position
-				transform.position = pos; 
+				transform.position = pos;
+			
 				timer_red = 0.0f;
+		renderer.enabled = true;
 
-//Mapping values to stressors
-
-
-		// HERE I COMENT 
-//				spawnDistance = ManagerScript.spawnDistance;
-//				//CoolDown = ManagerScript.CoolDown;    
-//				timer_red = ManagerScript.timer_red; 
-//				TimerForLooking = ManagerScript.TimerForLooking; 
-//				moveDistance = ManagerScript.moveDistance;  
-//				speed = ManagerScript.speed; 
 		}
 
 
@@ -263,27 +245,27 @@ public class SpawnLookRed : MonoBehaviour
 				renderer.enabled = false;
 		}
 
-	IEnumerator vibrateController ()
-	{
-		GamePad.SetVibration (0, 0.5f, 0.5f);
-		yield return new WaitForSeconds (1);
-		GamePad.SetVibration (0, 0.0f, 0.0f);
-	}
+		IEnumerator vibrateController ()
+		{
+				GamePad.SetVibration (0, 0.5f, 0.5f);
+				yield return new WaitForSeconds (1);
+				GamePad.SetVibration (0, 0.0f, 0.0f);
+		}
 
-	IEnumerator stun ()
-	{
-		pController = GameObject.Find ("OVRPlayerController");
-		OVRPlayerController controller = pController.GetComponent<OVRPlayerController> ();
-		controller.SetMoveScaleMultiplier(0.0f);
-		//--GameObject.Find ("Character").SendMessage ("changeMovement", false);
-		yield return new WaitForSeconds (1);
-		controller.SetMoveScaleMultiplier(3.0f);
-		//--GameObject.Find ("Character").SendMessage ("changeMovement", true);
-		//GameObject.Find ("Character").SendMessage ("changeMovement", true);
-		//characterMotor.enabled = true;
-		//character1.GetComponent<CharacterMotor>().enabled = true;
-		//GameObject.Find ("Character").SendMessage ("changeMovement", true);
-	}
+		IEnumerator stun ()
+		{
+				pController = GameObject.Find ("OVRPlayerController");
+				OVRPlayerController controller = pController.GetComponent<OVRPlayerController> ();
+				controller.SetMoveScaleMultiplier (0.0f);
+				//--GameObject.Find ("Character").SendMessage ("changeMovement", false);
+				yield return new WaitForSeconds (1);
+				controller.SetMoveScaleMultiplier (3.0f);
+				//--GameObject.Find ("Character").SendMessage ("changeMovement", true);
+				//GameObject.Find ("Character").SendMessage ("changeMovement", true);
+				//characterMotor.enabled = true;
+				//character1.GetComponent<CharacterMotor>().enabled = true;
+				//GameObject.Find ("Character").SendMessage ("changeMovement", true);
+		}
 	
 }
 
